0d3c888c9f9e50987e66b36f76a9d3eca52bef9e
sadikultra <sadikultra@gmail.com>
2020-04-22 14:48:45 +0600

    Added: ReadService

--- app.php
+++ app.php
@@ -3,17 +3,23 @@
 
 use Sadiq\CommandHandler;
 use Symfony\Component\Console\Application;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 require __DIR__ . '/vendor/autoload.php';
 
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+
 
 $command = new CommandHandler();
 $application = new Application();
 $application->add($command);
-$application->setDefaultCommand($command->getName());
+$application->setDefaultCommand($command->getName(), true);
 try {
     $application->run();
 } catch (Exception $e) {
--- src/ReaderService.php
+++ src/ReaderService.php
@@ -1,26 +1,56 @@
 <?php
 namespace Sadiq;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 class ReaderService {
 
     private $filesystem;
+    private $log;
 
     public function __construct()
     {
         $this->filesystem = new Filesystem();
+        // create a log channel
+        $this->log = new Logger('ReaderService');
+        $this->log->pushHandler(new StreamHandler(getenv('LOG_FILE'), Logger::WARNING));
     }
 
     public function readCurrencyData(){
         $fileName = getenv('CURRENCY_DATA_FILE');
         if ($this->filesystem->exists($fileName)){
             $rawJson = file_get_contents($fileName);
-
             return json_decode($rawJson);
         }else{
-//            log error, no currency data found
+            $this->log->error('CURRENCY_DATA_FILE: '. $fileName .', not found');
         }
     }
 
+    public function readRates(){
+            return $this->readApiToJson(getenv('RATES_API_ENDPOINT'))['rates'];
+    }
+
+    public function readBinList($bin){
+        return $this->readApiToJson(getenv('BIN_LIST_API_ENDPOINT') . $bin);
+    }
+
+    public function readApiToJson($apiEndPoint){
+        try{
+            return $rates = @json_decode(file_get_contents($apiEndPoint), true);
+        }catch (\Exception $exception){
+            $this->log->error($exception->getMessage());
+        }
+
+    }
+
+    public function readInputFile($fileName){
+        if ($this->filesystem->exists($fileName)){
+            return explode("\n", file_get_contents($fileName));
+        }else{
+            $this->log->error($fileName .', not found');
+            die;
+        }
+    }
 }
