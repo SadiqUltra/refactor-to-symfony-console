2795901d8c3948eec347c1488e0d9907248c7eba
sadikultra <sadikultra@gmail.com>
2020-04-22 14:08:27 +0600

    Init: Symfony Console

--- app.php
+++ app.php
@@ -2,7 +2,19 @@
 
 
 use Sadiq\CommandHandler;
+use Symfony\Component\Console\Application;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$d = new CommandHandler();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$command = new CommandHandler();
+$application = new Application();
+$application->add($command);
+$application->setDefaultCommand($command->getName());
+try {
+    $application->run();
+} catch (Exception $e) {
+}
--- src/CommandHandler.php
+++ src/CommandHandler.php
@@ -1,9 +1,23 @@
 <?php
 namespace Sadiq;
 
-class CommandHandler {
-    public function __construct()
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CommandHandler extends Command
+{
+    protected static $defaultName = 'refactor';
+
+    protected function configure()
     {
-        echo "..." . PHP_EOL;
+        $this->setDescription('refactor');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello World');
+
+        return 0;
     }
 }
